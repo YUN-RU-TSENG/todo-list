2cacd392824f0adeb1c71c2137908a970e1cc736
TSENG-YUN-RU <coco002197@gmail.com>
2021-05-23 15:34:34 +0800

    deploy

--- vue3/vue.config.js
+++ vue3/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/todo-list/" : "/",
   css: {
     loaderOptions: {
       /**
@@ -11,4 +12,4 @@ module.exports = {
       }
     }
   }
-}
+};
